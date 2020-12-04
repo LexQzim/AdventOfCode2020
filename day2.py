2b957491468a4cfbb2447a850be0f2dbf42becc3
SSC-OS\amizuk <a.mizuk@salt-and-pepper.eu>
2020-12-04 17:12:30 +0100

    finisches part 2 of day 2

--- day2.py
+++ day2.py
@@ -1,11 +1,7 @@
 with open('day2.txt') as file:
     entries = [entry.strip() for entry in file]
 
-minCount = []
-maxCount = []
-letter = []
-passphrase = []
-
+# part one
 countOfCorrectPassphrases = 0
 
 for entry in entries:
@@ -22,5 +18,30 @@ for entry in entries:
     if letterCount >= minCount and letterCount <= maxCount:
         countOfCorrectPassphrases += 1
 
+print('part one ' + str(countOfCorrectPassphrases))
 
-print(countOfCorrectPassphrases)
+# part two
+countOfCorrectPassphrases = 0
+
+for entry in entries:
+    firstPos = int(entry.split(' ', -1)[0].split('-', -1)[0]) - 1
+    secondPos = int(entry.split(' ', -1)[0].split('-', -1)[1]) - 1
+    letter = entry.split(' ', -1)[1][0]
+    passphrase = entry.split(' ', -1)[2]
+
+    letterFound = False
+    for i in range(0, len(passphrase)):
+        if i == firstPos and passphrase[i] == letter:
+            letterFound = True
+
+        if i == secondPos and passphrase[i] == letter:
+            if letterFound:
+                letterFound = False
+            else:
+                letterFound = True
+
+    if(letterFound):
+        countOfCorrectPassphrases += 1
+
+
+print('part two ' + str(countOfCorrectPassphrases))
