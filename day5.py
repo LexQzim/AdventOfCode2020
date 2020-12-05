e114d4af3617f5c0ea55d16bdbbdd7ab69ff8630
SSC-OS\amizuk <a.mizuk@salt-and-pepper.eu>
2020-12-05 11:02:32 +0100

    finished day 5 part two

--- day5.py
+++ day5.py
@@ -3,6 +3,7 @@ with open('day5.txt') as file:
 
 # Part One
 highestSeatId = 0
+listOfIds = []
 for entry in entries:
     row = range(128)
     col = range(8)
@@ -23,9 +24,29 @@ for entry in entries:
                 col = col[lenght:]
 
     seatId = row[0] * 8 + col[0]
+    listOfIds.append(seatId)
     if seatId > highestSeatId:
         highestSeatId = seatId
     # print(row[0])
     # print(col[0])
 
 print('Part One: highestSeatId= ' + str(highestSeatId))
+
+listOfIds.sort()
+leftNeighbour = 0
+rightNeighbour = 0
+for i in range(0, len(listOfIds)):
+    if i+1 < len(listOfIds):
+        if listOfIds[i+1] - listOfIds[i] > 1:
+            leftNeighbour = listOfIds[i]
+
+        if listOfIds[i] - listOfIds[i+1] < -1:
+            rightNeighbour = listOfIds[i+1]
+
+mySeat = 0
+if leftNeighbour < rightNeighbour:
+    mySeat = leftNeighbour + (rightNeighbour - leftNeighbour)/2
+else:
+    mySeat = rightNeighbour + (leftNeighbour - rightNeighbour)/2
+
+print("Part Two: My Seat Id = " + str(int(mySeat)))
