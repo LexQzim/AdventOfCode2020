9a1e97995c4997e1e83d2a975dc7b3e5cbb177ba
SSC-OS\amizuk <a.mizuk@salt-and-pepper.eu>
2020-12-11 20:10:13 +0100

    finisched day 7 part two

--- csharp/AdventOfCode2020/AdventOfCode2020/Day7/Bag.cs
+++ csharp/AdventOfCode2020/AdventOfCode2020/Day7/Bag.cs
@@ -6,18 +6,19 @@ namespace AdventOfCode2020
 {
     public class Bag
     {
-        public string color;
-        public List<Bag> containingBags = new List<Bag>();
+        public readonly string color;
+        public readonly List<Bag> containingBags = new List<Bag>();
         public List<int> amountOfBags = new List<int>();
+        public int sumOfBags = 0;
 
         public Bag(string color)
         {
             this.color = color;
         }
 
-        private bool ContainsColor(string color)
+        private bool ContainsColor(string bagColor)
         {
-            return containingBags.Any(bag =>bag.color.Equals(color));
+            return containingBags.Any(bag =>bag.color.Equals(bagColor));
         }
         
         public bool ContainsShinyGold()
--- csharp/AdventOfCode2020/AdventOfCode2020/Day7/Day7.cs
+++ csharp/AdventOfCode2020/AdventOfCode2020/Day7/Day7.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace AdventOfCode2020
 {
     public class Day7
     {
         private readonly List<Bag> bagContainer = new List<Bag>();
-        
+
+        #region Init
+
         public void ReadData()
         {
             var counter = 0;
@@ -20,30 +22,30 @@ namespace AdventOfCode2020
             while ((line = file.ReadLine()) != null)
             {
                 var bag = CreateOrReturn(line.Split("contain")[0].Replace("bags", "").Trim());
-                
+
                 PackMyBag(bag, line.Split("contain")[1]);
                 counter++;
-                
             }
 
             file.Close();
             Console.WriteLine("There were {0} lines.", counter);
             Console.WriteLine("There were {0} bags.", bagContainer.Count);
-       
-            // foreach (var bag in bagContainer)
-            // {
-            //     Console.WriteLine("{0}", bag.color);
-            //
-            //     Console.WriteLine("Contains: {0}", bag.containingBags.Count);
-            //     for (int i = 0; i < bag.containingBags.Count; i++)
-            //     {
-            //         Console.WriteLine("- {1} {0}", bag.containingBags[i].color, bag.amountOfBags[i]);
-            //     }
-            // }
 
+            foreach (var bag in bagContainer)
+            {
+                Console.WriteLine("{0} contains {1} bags:", bag.color, bag.sumOfBags);
+            
+                for (var index = 0; index < bag.containingBags.Count; index++)
+                {
+                    Console.WriteLine("- {1} {0}", bag.containingBags[index].color, bag.amountOfBags[index]);
+                }
+            }
+            
             FindAmountOfGoldBags();
-
+            
+            PartTwo();
         }
+
 
         private Bag CreateOrReturn(string color)
         {
@@ -54,31 +56,35 @@ namespace AdventOfCode2020
                 newBag = new Bag(color);
                 bagContainer.Add(newBag);
             }
-            
+
             return newBag;
         }
-        
+
+        #endregion
+
+        #region Part One
+
         private void FindAmountOfGoldBags()
         {
-            Console.WriteLine(bagContainer.Count(bag => ContainsGoldBag(bag.containingBags)));
+            Console.WriteLine("{0} bags contains at least one shiny gold bag!",
+                bagContainer.Count(bag => ContainsGoldBag(bag.containingBags)));
         }
-        
+
         private static bool ContainsGoldBag(IReadOnlyCollection<Bag> bagList)
         {
             if (bagList.Count == 0) return false;
             foreach (var bag in bagList)
             {
                 if (bag.IsShinyGold()) return true;
-                
+
                 if (bag.ContainsShinyGold()) return true;
 
                 if (ContainsGoldBag(bag.containingBags)) return true;
-
             }
 
             return false;
         }
-        
+
         private void PackMyBag(Bag mainBag, string bagStrings)
         {
             var bagString = bagStrings.Split(",");
@@ -86,15 +92,32 @@ namespace AdventOfCode2020
             {
                 var listOfString = bs.Trim().Split(" ");
                 var count = listOfString[0];
-                
-                if(count == "no")
+
+                if (count == "no")
                     continue;
-                
+
                 var bag = CreateOrReturn(listOfString[1] + " " + listOfString[2]);
-                
+
                 mainBag.containingBags.Add(bag);
                 mainBag.amountOfBags.Add(int.Parse(count));
+                mainBag.sumOfBags += int.Parse(count);
             }
         }
+
+        #endregion
+
+        #region Part Two
+
+        private void PartTwo()
+        {
+            Console.WriteLine("GoldBag Sums: {0}", CountBags(bagContainer.Find(bag => bag.color == "shiny gold")));
+        }
+
+        private static int CountBags(Bag parent)
+        {
+            return parent.containingBags.Select((t, i) => parent.amountOfBags[i] + parent.amountOfBags[i] * CountBags(t)).Sum();
+        }
+
+        #endregion
     }
 }
